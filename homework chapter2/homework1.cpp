1d0d55a9548f80610a2f4825e551beebf600b50b
Duangporn Sumamal <6106021630015@fitm.kmutnb.ac.th>
2020-07-16 00:39:53 +0700

    first commit

---
+++ homework chapter2/homework1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void main()
+{
+	cout<<" \n";
+	cout<<" 1st Term of 2020 \t\\45132114  Wanthanee Prachuabsupakij \n";
+	cout<<" \n";
+	cout<<"---------------------------------------------------------------------------------------------\n";
+	cout<<"|\tSubject ID\t|\tSubject name\t|\tScore (100)\t|\tGrad\t|\n";
+	cout<<"---------------------------------------------------------------------------------------------\n";
+    cout<<"|\t4570034\t\t|Introduct to computer\t|\t\85\t\t|\t\"A\"\t|\t\n";
+	cout<<" \n";
+	cout<<"|\t4580247\t\t|English\t\t|\t74\t\t|\t\"B\"\t|\t\n";
+	cout<<" \n";
+	cout<<"|\t4530001\t\t|Design Thinking\t|\t92\t\t|\t\"A\"\t|\t\n";
+	cout<<"---------------------------------------------------------------------------------------------\n";
+	cout<<"|\tTOTAL\t\t|Subject = 3\t\t|\tAverage = 3.87\t|\t\n";
+	cout<<"=============================================================================================\n";
+    
+} 
